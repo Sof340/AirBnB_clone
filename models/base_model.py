0cb16a383b057d3aa39eeac914431a5f327d726d
Sof340 <sofianeadrar34@gmail.com>
2024-05-19 15:12:07 -0700

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,10 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
+                    value = datetime.datetime.fromisoformat(value)
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
