30a71ecafda849955f4216910c96a1a95c1d8613
Sof340 <sofianeadrar34@gmail.com>
2024-05-19 14:31:07 -0700

    third test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,3 +1,5 @@
+import io
+import sys
 import unittest
 from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
@@ -18,3 +20,19 @@ class TestBaseModel(unittest.TestCase):
         value_now = base_1.updated_at
         base_1.save()
         self.assertNotEqual(value_now, base_1.updated_at)
+
+    def test_str(self):
+        """
+        Let test if the correct ouput is printed using __str__.
+        """
+        base_1 = BaseModel()
+        correct_output = "[{}] ({}) {}".format(base_1.__class__.__name__, base_1.id, base_1.__dict__) + '\n'
+
+        output_buffer = io.StringIO()
+        sys.stdout = output_buffer
+        print(base_1)
+        sys.stdout = sys.__stdout__
+
+        captured_output = output_buffer.getvalue()
+        captured_output = captured_output
+        self.assertMultiLineEqual(captured_output, correct_output)
